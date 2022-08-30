0fcd8934c16a1c8f84663d284bd245d09667a576
adolfo nin <ionfiz92@gmail.com>
2022-08-29 22:02:30 -0300

    NavBar Css Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import logo from './logo.svg';
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <>
-    <NavBar />
-    <ItemListContainer texto='NexusBroRental'/>
-    </>
-    
+   
+    <div className="App">
+      
+      <NavBar />
+      <ItemListContainer texto='NexusBroRental'/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+        <p>
+          Proyecto Nexus Bro Rental <code>src/App.js</code> Adolfo Nin
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import '../CartWidget/CartWidget'
-import CartWidget from "../CartWidget/CartWidget";
+import logo from '../../logo.svg';
 import './styles.css';
+import CartWidget from "../CartWidget/CartWidget";
+
 
 export const NavBar = () =>{
     return (
         <div classname="navBar-container">
             <nav className="nav">
                 <div className="nav_brand">
-                    <a className="nav_link" href="#">MiMarca</a>
+                    <a className="nav_link" href="#">
+                        <img width={'100px'} src={logo} alt="logo" />
+                    </a>
+
                 </div>
                 <ul classname="nav_list">
                     <li>
