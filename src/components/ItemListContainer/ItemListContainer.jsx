a8ed0bfd85e88d6902cab2be19df0b45122ac62d
adolfo nin <ionfiz92@gmail.com>
2022-08-31 21:12:16 -0300

    desafio Contador con botón

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,21 @@
+import ItemCount from "../ItemCount/ItemCount";
 import React from "react";
 import Title from '../Title/Title';
 
 export const ItemListContainer = ({texto}) => {
+
+    const onAdd = (quantity) =>{
+        console.log(`Compraste ${quantity} unidades `);
+    }
+    
     return (
+        <>
         <Title greeting={texto} />
+        <ItemCount initial={1} stock={5} onAdd={onAdd} />
+        </>
     );
 }
 
 export default ItemListContainer;
+
+
