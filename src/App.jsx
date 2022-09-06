322637fbe667c282e6063631c1889ffbca2a7e08
adolfo nin <ionfiz92@gmail.com>
2022-09-05 22:23:51 -0300

    Catálogo con MAPS y Promises

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,7 @@ function App() {
       <NavBar />
       <ItemListContainer texto='NexusBroRental'/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-        <p>
-          Proyecto Nexus Bro Rental <code>src/App.js</code> Adolfo Nin
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,17 +1,36 @@
+import React, {useState,useEffect} from "react";
+
 import ItemCount from "../ItemCount/ItemCount";
-import React from "react";
+import ItemList from "../ItemList/ItemList";
 import Title from '../Title/Title';
 
+const films = [
+    {id: 1, image: "http://gluckrental.com/img/product/xone92.jpg", title: "mixer pioneer"},
+    {id: 2, image: "http://gluckrental.com/img/product/nexus2000_2.jpg", title: "cdj pioneer"},
+    {id: 3, image: "http://gluckrental.com/img/product/sl1200mk3.jpg", title: "technics turntable "},
+];
+
 export const ItemListContainer = ({texto}) => {
+    const [data, SetData] = useState([]);
+
+    useEffect (() => {
+        const getData = new Promise(resolve =>{
+            setTimeout(() => {
+                resolve(films);
+            },3000);
+        });
+        getData.then(res => SetData(res));
+    },[])
 
     const onAdd = (quantity) =>{
-        console.log(`Compraste ${quantity} unidades `);
+        console.log(`Compraste ${quantity} unidades`);
     }
     
     return (
         <>
         <Title greeting={texto} />
         <ItemCount initial={1} stock={5} onAdd={onAdd} />
+        <ItemList data={data} />
         </>
     );
 }
