0fa399841c10642631c0db11700d848738ab8507
adolfo nin <ionfiz92@gmail.com>
2022-09-12 20:18:50 -0300

    add cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import ItemList from './components/ItemList/ItemList';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Cart from './components/Cart/Cart';
 
 
 function App() {
   return (
    <>
     <NavBar />
+    <ItemListContainer />
     <ItemDetailContainer />
+    <Cart />
    </>  
   );
 }
