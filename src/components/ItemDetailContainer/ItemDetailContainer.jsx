52b08d1feb61187e4873d2727365349af1087c66
adolfo nin <ionfiz92@gmail.com>
2022-09-12 23:25:27 -0300

    primera entrega del proyecto final

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,18 +1,29 @@
-import ItemDetail from '../ItemDetail/ItemDetail';
 import React, { useState, useEffect } from 'react';
 
-const films = {id: 1, image: "http://gluckrental.com/img/product/xone92.jpg", title: "mixer pioneer"};
+import ItemDetail from '../ItemDetail/ItemDetail';
+import { useParams } from 'react-router-dom';
 
+
+const films = [  
+{id: 1, image: "http://gluckrental.com/img/product/xone92.jpg", category:"Mixers", title: "Xone 92"},
+{id: 2, image: "http://gluckrental.com/img/product/nexus2000_2.jpg", category:"CDJ", title: "Nexus 2000"},
+{id: 3, image: "http://gluckrental.com/img/product/sl1200mk3.jpg", category:"CDJ", title: "skt 1200"},
+{id: 4, image: "http://gluckrental.com/img/product/ev15ekx.jpg", category:"Parlantes", title: "Electrovoice EKX 15 "},
+{id: 5, image: "http://gluckrental.com/img/product/vantecpack.jpg", category:"Parlantes", title: "Das Vantec Combo "},
+{id: 6, image: "http://gluckrental.com/img/product/funktionone_revo2.jpg", category:"Parlantes", title: "Funktion-one Resolution 2"},
+];
 
 export const ItemDetailContainer = () => {
     const [data, SetData] = useState({});
+    const { detalleId } = useParams();
+
     useEffect (() => {
         const getData = new Promise(resolve => {
             setTimeout(() => {
                 resolve(films);
             }, 3000);
         });
-        getData.then(res => SetData(res));
+        getData.then(res => SetData(res.find(film => film.id === parseInt(detalleId))));
     },[])
 
   return (
