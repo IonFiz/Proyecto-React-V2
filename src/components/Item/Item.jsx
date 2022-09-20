ce74bfa4d0cfac56373bda23fdd423047a1c2a53
adolfo nin <ionfiz92@gmail.com>
2022-09-19 23:06:54 -0300

    DesafioCartContext.v5

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,12 +1,12 @@
 import './style.css';
 
 import { Link } from 'react-router-dom';
-import React, {useContext} from 'react';
-import { CartContext } from '../../context/CartContext';
+import React from 'react';
+
 
 const Item = ({info}) => {
 
-  const nombre = useContext(CartContext);
+  
     return (
       <Link to={`/detalle/${info.id}`} className="film">
           <img src={info.image} alt="" />
