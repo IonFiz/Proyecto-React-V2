98ded024a76fbf8cf947f1c925469f2ae27ac4fd
adolfo nin <ionfiz92@gmail.com>
2022-09-19 21:44:05 -0300

    DesafioCartContext.v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Cart from './components/Cart/Cart';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
 
 
-
+export const CartContext = React.createContext('');
 
 function App() {
   return (
    <>
     <BrowserRouter>
       <NavBar />
+      <CartContext.Provider value='Nexus'>
       <Routes>
         <Route path='/' element={<ItemListContainer />} />
         <Route path='/categoria/:categoriaId' element={<ItemListContainer />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/detalle/:detalleId' element={<ItemDetailContainer />} />
       </Routes>
+      </CartContext.Provider>
     </BrowserRouter>  
    </>
   );
